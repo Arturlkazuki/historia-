852eaad61b3be0171a9b2e22b4b2c7b657ab01b9
Arturlkazuki <machado.artur@escola.pr.gov.br>
2026-09-15 12:21:32 -0300

    chbn zjdj

--- js/pergunta.js
+++ js/pergunta.js
@@ -1,13 +1,4 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
-
-
-
-const perguntas = 
-[
+export const perguntas = [
     {
         enunciado: "É o dia da grande final do campeonato escolar de vôlei. O ginásio está lotado e o técnico chama você para entrar no primeiro set como titular. Qual o seu primeiro pensamento?",
         alternativas: [
@@ -114,46 +105,3 @@ const perguntas =
         ]
     },
 ];
-
-let atual = 0; 
-let perguntaAtual;
-let historiaFinal = "";
-
-function mostraPergunta() {
-    if(atual >= perguntas.length){
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
-}
-
-function mostraAlternativas(){
-    for(const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild(botaoAlternativas);
-    }
-}
-
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao;)
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
-}
-
-function mostraResultado(){
-    caixaPerguntas.textContent = "Em 2049...";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = ""; 
-}
-function aleatorio(lista){
-    const posicao = Math.random()*lista.length
-    console.log(posicao)
-}
-
-mostraPergunta();
